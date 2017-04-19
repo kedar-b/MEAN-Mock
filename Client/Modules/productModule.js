1a8998e94b333834614204477710492eef9f5d04
Rohan <rohan.deshpande@kpit.com>
2017-04-19 12:04:23 +0530

    module name changed

--- Client/Modules/productModule.js
+++ Client/Modules/productModule.js
@@ -1,1 +1,31 @@
-var productApp = angular.module('productApp');
+var productApp = angular.module('productApp',["ngRoute"]);
+
+productApp.config(function($routeProvider,$locationProvider){
+    // $routeProvider.when('/',
+    // {
+    //     templateUrl : "Views/index.htm"
+    // })
+    $routeProvider.when('/login',
+    {
+        templateUrl : "Views/UserManagement/login.htm",
+        controller : "loginUserController"
+    })
+    .when('/register',
+    {
+        templateUrl : "Views/UserManagement/register.htm",
+        controller : "registerUserController"//,
+        //controllerAs : 'login'
+    })
+    .when('/viewproduct',
+    {
+        templateUrl : "Views/ProductManagement/displayProduct.html",
+        controller : "productController"
+    })
+    .otherwise(
+    {
+        template : "<h1>Page Not Found</h1>"
+    })
+
+    // get rid of the hash in the URL
+    $locationProvider.html5Mode(true);
+})
